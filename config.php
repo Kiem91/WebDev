2790fd352991a352cf55116fcd4618d59b4f7f3f
Michael Davis <michaelrdavis91@gmail.com>
2020-01-07 07:36:11 -0500

    fajsd;foij

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'shareddb-r.hosting.stackcp.net');
+   define('DB_USERNAME', 'UserPRD-3132339d6e');
+   define('DB_PASSWORD', 'sxwmxqpzmf1');
+   define('DB_DATABASE', 'UserPRD-3132339d6e');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
